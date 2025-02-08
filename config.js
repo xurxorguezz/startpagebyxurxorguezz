c8c4fe47748f6f90b9ec4b06b67e88e595d4020a
xurxorguezz <eljurjoo@gmail.com>
2025-02-08 18:11:41 +0100

    uploaded all project

---
+++ config.js
@@ -0,0 +1,209 @@
+const CONFIG = {
+
+	name: 'Xurxo',
+	imageBackground: false,
+	openInNewTab: true,
+	twelveHourFormat: false,
+
+	
+	greetingMorning: 'Buenos Días,',
+	greetingAfternoon: 'Buenas Tardes,',
+	greetingEvening: 'Buenas Noches,',
+	greetingNight: 'Hora de Dormir!',
+
+	
+	bentoLayout: 'bento', 
+	
+	weatherKey: 'eb372169db9de4cbbeccb4ce1f37c457 ', 
+	weatherIcons: 'OneDark', 
+	weatherUnit: 'C', 
+	language: 'es',
+
+	trackLocation: true, 
+	defaultLatitude: '42.082',
+	defaultLongitude: '-8.499',
+
+	autoChangeTheme: true,
+
+	changeThemeByOS: true,
+
+	changeThemeByHour: false,
+	hourDarkThemeActive: '18:30',
+	hourDarkThemeInactive: '07:00',
+
+
+
+	firstButtonsContainer: [
+		{
+			id: '1',
+			name: 'Github',
+			icon: 'github',
+			link: 'https://github.com/',
+		},
+		{
+			id: '2',
+			name: 'Gmail',
+			icon: 'mail',
+			link: 'https://gmail.com/',
+		},
+		{
+			id: '5',
+			name: 'Spotify',
+			icon: 'headphones',
+			link: 'https://open.spotify.com',
+		},
+		{
+			id: '4',
+			name: 'Calendar',
+			icon: 'calendar',
+			link: 'https://calendar.google.com/calendar/r',
+		},
+		{
+			id: '3',
+			name: 'AulaVirtual',
+			icon: 'trello',
+			link: 'https://www.edu.xunta.gal/centros/iessalvaterramino/aulavirtual/',
+		},
+		{
+			id: '6',
+			name: 'Youtube',
+			icon: 'youtube',
+			link: 'https://youtube.com/',
+		},
+	],
+
+	secondButtonsContainer: [
+		{
+			id: '1',
+			name: 'Music',
+			icon: 'headphones',
+			link: 'https://open.spotify.com',
+		},
+		{
+			id: '2',
+			name: 'twitter',
+			icon: 'twitter',
+			link: 'https://twitter.com/',
+		},
+		{
+			id: '3',
+			name: 'bot',
+			icon: 'bot',
+			link: 'https://discord.com/app',
+		},
+		{
+			id: '4',
+			name: 'Amazon',
+			icon: 'shopping-bag',
+			link: 'https://amazon.com/',
+		},
+		{
+			id: '5',
+			name: 'Hashnode',
+			icon: 'pen-tool',
+			link: 'https://hashnode.com/',
+		},
+		{
+			id: '6',
+			name: 'Figma',
+			icon: 'figma',
+			link: 'https://figma.com/',
+		},
+	],
+
+
+
+	firstlistsContainer: [
+		{
+			icon: 'coffee',
+			id: '2',
+			links: [
+				{
+					name: 'Google',
+					link: 'https://www.google.com/',
+				},
+				{
+					name: 'Instagram',
+					link: 'https://www.instagram.com/',
+				},
+				{
+					name: 'Whatsapp',
+					link: 'https://web.whatsapp.com/',
+				},
+				{
+					name: 'Discord',
+					link: 'https://discord.com/channels/@me/',
+				},
+			],
+		},
+		{
+			icon: 'compass',
+			id: '1',
+			links: [
+				{
+					name: '@xurxorguezz_',
+					link: 'https://xurxorguezz.github.io',
+				},
+				{
+					name: 'Toolify',
+					link: 'https://toolify.ai/',
+				},
+				{
+					name: 'Bible',
+					link: 'https://www.bible.com/es/bible/',
+				},
+				{
+					name: 'Quote Of The Day',
+					link: 'https://proverbia.net/frase-del-dia',
+				},
+			],
+		},
+	],
+
+	secondListsContainer: [
+		{
+			icon: 'binary',
+			id: '1',
+			links: [
+				{
+					name: 'Spotify',
+					link: 'https://www.spotify.com',
+				},
+				{
+					name: 'Reddit',
+					link: 'https://www.reddit.com',
+				},
+				{
+					name: 'Hashnode',
+					link: 'https://www.hashnode.com',
+				},
+				{
+					name: 'Pocket',
+					link: 'https://www.pocket.com',
+				},
+			],
+		},
+		{
+			icon: 'github',
+			id: '2',
+			links: [
+				{
+					name: 'Front',
+					link: 'https://www.reddit.com/r/Frontend/',
+				},
+				{
+					name: 'Rust',
+					link: 'https://www.reddit.com/r/rust/',
+				},
+				{
+					name: 'Go',
+					link: 'https://www.reddit.com/r/golang/',
+				},
+				{
+					name: 'Repos',
+					link: 'https://github.com/xurxorguezz',
+				},
+			],
+		},
+	],
+};
